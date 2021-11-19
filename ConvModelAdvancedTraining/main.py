baeab7c500f6ae95896c6d9b02b7991910a5dd32
akayal <amit.kayal@csgi.com>
2021-11-20 01:39:23 +0530

    fixed syntax issue

--- ConvModelAdvancedTraining/main.py
+++ ConvModelAdvancedTraining/main.py
@@ -72,7 +72,8 @@ def run_experiments(lr = 0.1, resume = '', description = 'PyTorchCIFAR10Training
 # net = VGG('VGG19')
   net = ResNet18()
   net = net.to(device)
-  model_summary(net, input_size=(3, 32, 32)):
+  
+  model_summary(net, input_size=(3, 32, 32))
       
   exp_metrics={}
   if device == 'cuda':
