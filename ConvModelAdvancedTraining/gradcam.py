759894e41c4e3e95c35b098a3f2b54255467fbdc
akayal <amit.kayal@csgi.com>
2021-11-21 19:16:31 +0530

    added

--- ConvModelAdvancedTraining/gradcam.py
+++ ConvModelAdvancedTraining/gradcam.py
@@ -145,6 +145,18 @@ def generate_gradcam(misclassified_images, model, target_layers,device):
     gcam.remove_hook()
     return layers, probs, ids
 
+def unnormalize(img):
+    mean = (0.49139968, 0.48215841, 0.44653091)
+    std = (0.24703223, 0.24348513, 0.26158784)
+#   mean,std = calculate_mean_std("CIFAR")
+    img = img.cpu().numpy().astype(dtype=np.float32)
+  
+    for i in range(img.shape[0]):
+        img[i] = (img[i]*std[i])+mean[i]
+  
+    return np.transpose(img, (1,2,0))
+
+
 def plot_gradcam(gcam_layers, target_layers, class_names, image_size,predicted, misclassified_images):
     
     images=[]
