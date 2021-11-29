9cca1b5e7264c28887b6adf6aeafe5eb773b7d96
akayal <amit.kayal@csgi.com>
2021-11-29 14:47:22 +0530

    changed

--- ConvModelAdvancedTraining/utils.py
+++ ConvModelAdvancedTraining/utils.py
@@ -373,10 +373,9 @@ def data_albumentations_customresnet(mean,std, horizontalflip_prob = 0.2,
 
     return lambda img:train_transforms(image=np.array(img))["image"],lambda img:test_transforms(image=np.array(img))["image"]
 
-def data_albumentations_customresnet_fc(mean,std, max_holes=4, horizontalflip_prob = 0.2,
+def data_albumentations_customresnet_fc(mean,std, max_holes=4,horizontalflip_prob = 0.2,
                         rotate_limit = 15,
                         shiftscalerotate_prob = 0.25,
-                        num_holes = 2,
                         random_crop_p = 0.2,
                         cutout_prob = 0.8,
                         PAD=4):
@@ -411,7 +410,7 @@ def data_albumentations_customresnet_fc(mean,std, max_holes=4, horizontalflip_pr
          A.HorizontalFlip(p=0.5),
          # Cutout
          A.CoarseDropout(
-             max_holes=4,
+             max_holes=max_holes,
              max_height=8,
              max_width=8,
              min_holes=1,
